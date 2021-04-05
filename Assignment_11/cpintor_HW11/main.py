cdea585a065ceec390566b2798f081af8939cd54
Cristian Pintor <cpintor@hawk.iit.edu>
2021-04-04 23:34:56 -0500

    Adding output files from data

--- Assignment_11/cpintor_HW11/main.py
+++ Assignment_11/cpintor_HW11/main.py
@@ -6,8 +6,6 @@ Name: Cristian Pintor
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import csv
-import numpy as np
 
 # Importing file
 workers = pd.read_csv('/Users/crissyp/projects/ITMD-413/ITMD_413/Assignment_11/cpintor_HW11/workerstips.csv')
@@ -82,3 +80,51 @@ plt.ylabel('Passengers')
 plt.show()
 
 # f
+data = pd.read_csv('titanic.csv')
+df = pd.DataFrame(data)
+
+women = []
+men = []
+children = []
+who = []
+
+w = 0
+ma = 0
+c = 0
+
+f = df.loc[df['Sex'] == 'female']
+m = df.loc[df['Sex'] == 'male']
+
+child = df.loc[df['Age'] < 18]
+woman = f.loc[f['Age'] >= 18]
+man = m.loc[m['Age'] >= 18]
+
+women = woman['Age'].values.tolist()
+men = man['Age'].values.tolist()
+children = child['Age'].values.tolist()
+
+for line in women:
+    w +=1
+    who.append('Women')
+
+for line in men:
+    ma +=1
+    who.append('Men')
+
+for line in children:
+    c +=1
+    who.append('Children')
+
+# new df with women, men and children divisions
+tWho = pd.DataFrame(who)
+
+# concatenate new df with existing dataset
+titanic_data = pd.concat([titanic, tWho], ignore_index=True, axis=1)
+titanic_index = titanic_data.rename(columns={0: 'PassengerId', 1: 'Survived', 2: 'Class', 3: 'Name', 4: 'Sex', 5: 'Age', 6: 'SibSp', 7: 'Parch', 8: 'Ticket', 9: 'Fare', 10: 'Cabin', 11: 'Embarked', 12: 'Who'})
+print(titanic_index.tail())
+titanic_final = titanic_index.dropna(subset=['Who'])
+print(titanic_final.tail())
+
+# plot graphs
+fx = sns.catplot(x='Class', col='Survived', data=titanic_final, hue='Who', kind='count')
+plt.show()
