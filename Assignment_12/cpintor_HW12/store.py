3f8c7e2cb027f573e563510fde74b16e5638e27c
Cristian Pintor <cpintor@hawk.iit.edu>
2021-04-11 22:53:02 -0500

    First part of hw assignment

--- Assignment_12/cpintor_HW12/store.py
+++ Assignment_12/cpintor_HW12/store.py
@@ -5,9 +5,48 @@ and services provided by these stores
 
 Name: Cristian Pintor
 '''
+from abc import ABC, abstractmethod
 
 
-# class Store:
+class Store:
+    def __init__(self, name, address, availability, taxes):
+        self.name = name
+        self.address = address
+        self.availability = availability
+        self.taxes = taxes
 
+    def get_name(self):
+        return self.name
 
+    def get_address(self):
+        return self.address
 
+    def get_availability(self):
+        return self.availability
+
+    def get_taxes(self):
+        return self.taxes
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_address(self, address):
+        self.address = address
+
+    def set_availability(self, availability):
+        self.availability = availability
+
+    def set_taxes(self, taxes):
+        self.taxes = taxes
+
+    def calculate_total_sales_tax(self):
+        pass
+
+    def calculate_total_sales(self):
+        pass
+
+    def is_store_open(self, availability):
+        if availability == 'open':
+            return 'true'
+        else:
+            return 'false'
